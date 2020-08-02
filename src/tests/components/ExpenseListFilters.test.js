e2fef8b03a676b3e817e733a5ead56c341e9060c
Andrew Andrewsson <sidor@adamed.com>
2020-08-02 14:58:04 +0200

    add tests for ExpenseFilters

---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { ExpenseListFilters } from '../../components/ExpenseListFilters'
+import { emptyFilters, setFilters } from '../fixtures/filters'
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper
+
+beforeEach(() => {
+    setTextFilter = jest.fn()
+    sortByDate = jest.fn()
+    sortByAmount = jest.fn()
+    setStartDate = jest.fn()
+    setEndDate = jest.fn()
+    wrapper= shallow(
+        <ExpenseListFilters
+            filters={emptyFilters}
+            setTextFilter={setTextFilter}
+            sortByDate={sortByDate}
+            sortByAmount={sortByAmount}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+        />)
+})
+
+test('Should render ExpenseListFilters correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('Should render ExpenseListFilters with setFilters correctly', () => {
+    wrapper.setProps({
+        filters: setFilters
+    })
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('Should handle setTextFilter correctly', () => {
+    const newText = 'water'
+    wrapper.find('input').at(0).simulate('change', { target : { value: newText }})
+    expect(setTextFilter).toHaveBeenLastCalledWith(newText)
+})
+
+test('Should handle sortByDate correctly', () => {
+    wrapper.find('select').simulate('change', { target: { value: 'date' } })
+    expect(sortByDate).toHaveBeenCalled()
+})
+
+test('Should handle sortByAmount correctly', () => {
+    wrapper.find('select').simulate('change', { target: { value: 'amount' } })
+    expect(sortByAmount).toHaveBeenCalled()
+})
+
+test('Should handle date change', () => {
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate: setFilters.startDate, endDate: setFilters.endDate})
+    expect(setStartDate).toHaveBeenLastCalledWith(setFilters.startDate)
+    expect(setEndDate).toHaveBeenLastCalledWith(setFilters.endDate)
+})
+
+test('Should handle focus change', () => {
+    wrapper.find('DateRangePicker').prop('onFocusChange')('endDate')
+    expect(wrapper.state('calendarFocused')).toBe('endDate')
+})
