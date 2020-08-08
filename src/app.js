98d0f744a69d33da48687ee8866827f1501ad610
Andrew Andrewsson <sidor@adamed.com>
2020-08-08 22:50:34 +0200

    remove mock data

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './components/routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense, removeExpense, editExpense } from './actions/expenses'
-import { setTextFilter, setEndDate, setStartDate, sortByAmount, sortByDate} from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
+// import { addExpense, removeExpense, editExpense } from './actions/expenses'
+// import { setTextFilter, setEndDate, setStartDate, sortByAmount, sortByDate} from './actions/filters'
+// import getVisibleExpenses from './selectors/expenses'
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -13,9 +13,9 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.dispatch(addExpense({description: 'Water bill', amount: 300}))
-store.dispatch(addExpense({description: 'rent', amount: 1300, createdAt: 9999999}))
-store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 100}))
+// store.dispatch(addExpense({description: 'Water bill', amount: 300}))
+// store.dispatch(addExpense({description: 'rent', amount: 1300, createdAt: 9999999}))
+// store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 100}))
 
 const jsx = (
     <Provider store={store}>
