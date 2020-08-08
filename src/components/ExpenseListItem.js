856985dd22b59249ff6e549d46cf69e9daa17573
Andrew Andrewsson <sidor@adamed.com>
2020-08-08 20:34:11 +0200

    added number and date formatter

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({id, description, note, amount, createdAt}) => {
     return (
@@ -8,9 +9,9 @@ const ExpenseListItem = ({id, description, note, amount, createdAt}) => {
             <Link to={`/edit/${id}`}>
                 <h3>Description:{description}</h3>
             </Link> 
-            <p>Amount:{amount}</p>
+            <p>Amount:{numeral(amount / 100).format('$0,0.00')}</p>
             <p>Note:{note}</p>
-            <p>Creation Time:{createdAt}</p>
+            <p>Creation Time:{moment(createdAt).format('Do MMMM YYYY')}</p>
         </div>
     )
 }
