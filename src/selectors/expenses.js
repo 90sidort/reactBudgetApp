5f7a93674fcfd3ddc31386ddc1dea5ea1f194db0
unknown <90sidort@gmail.com>
2020-08-26 23:52:12 +0200

    working range

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -6,9 +6,10 @@ const getVisibleExpenses = (expenses, { text, minAmount, maxAmount, sortBy, star
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true
         const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true
         const textMatch = (expense.description).toLowerCase().includes(text.toLowerCase())
-        const amountMatch = ((expense.amount) / 100) >= minAmount && ((expense.amount) / 100) <= maxAmount  
+        const minAmountMatch = ((expense.amount) / 100) >= minAmount
+        const maxAmountMatch = maxAmount === 10000 ? true : ((expense.amount) / 100) <= maxAmount  
 
-        return startDateMatch && endDateMatch && textMatch && amountMatch
+        return startDateMatch && endDateMatch && textMatch && minAmountMatch && maxAmountMatch
     }).sort((a, b) => {
         if (sortBy === 'date') {
             return a.createdAt < b.createdAt ? 1 : -1
